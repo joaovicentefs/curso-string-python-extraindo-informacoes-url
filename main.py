e3b257cb5ad28235fc627b40c083a931a660ac2d
Joao Vicente <joao@upsites.digital>
2021-07-30 16:24:40 -0300

    Aprendendo o uso dos métodos find() e len() no fatiamento e extração de dados de strings

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
+indice_interrogacao = url.find('?')# O método find retorna a posição do PRIMEIRO caracter da string informada.
+url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[20:]
+url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
+
+# Para pegar o valor do parametro moedaOrigem
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
