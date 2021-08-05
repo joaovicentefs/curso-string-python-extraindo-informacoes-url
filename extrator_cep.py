72a779b2c4a8d5186d9a73d69f42484fbee40883
Joao Vicente <joao@upsites.digital>
2021-08-04 23:38:29 -0300

    Simplificando a expressão regular usando quantificadores,intervalos e limitadores

--- extrator_cep.py
+++ extrator_cep.py
@@ -2,8 +2,8 @@ import re
 
 endereco = 'Rua das Flores, 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120'
 
-padrao = re.compile('[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]')
+padrao = re.compile('[0-9]{5}[-]{0,1}[0-9]{3}')
 busca = padrao.search(endereco) # Retorna uma Classa Match
 if busca:
-    cep = busca.group()
+    cep = busca.group() # Retorna a substring encontrada no Match(search)
     print(cep)
