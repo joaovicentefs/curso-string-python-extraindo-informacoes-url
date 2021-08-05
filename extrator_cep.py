d1008af994d42229d977a70e213f2799a6c9f85d
Joao Vicente <joao@upsites.digital>
2021-08-04 23:31:45 -0300

    Aprendendo a extrair dados usando expressões regulares, ainda de maneira bem inciial

---
+++ extrator_cep.py
@@ -0,0 +1,9 @@
+import re
+
+endereco = 'Rua das Flores, 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120'
+
+padrao = re.compile('[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]')
+busca = padrao.search(endereco) # Retorna uma Classa Match
+if busca:
+    cep = busca.group()
+    print(cep)
