3e55f20be024640e5c2ca62838d102a401420025
Joao Vicente <joao@upsites.digital>
2021-08-08 19:11:13 -0300

    Aprendendo um pouco mais sobre métodos especiais e implementando o __len__ na nossa classe

--- extrator_url.py
+++ extrator_url.py
@@ -38,7 +38,11 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
+
 url = 'bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar'
 extrator_url = ExtratorURL(url)
 valor_quantidade = extrator_url.get_valor_parametro('quantidade')
 print(valor_quantidade)
+print('A URL informada tem',len(extrator_url), 'caracteres.')
